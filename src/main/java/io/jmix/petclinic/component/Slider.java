1c2b2be5ff3b9606ad853fab450be6613ffb2d59
budarov <a.budarov@haulmont.com>
2025-04-21 19:50:19 +0400

    jquery Slider integration sample

---
+++ src/main/java/io/jmix/petclinic/component/Slider.java
@@ -0,0 +1,66 @@
+package io.jmix.petclinic.component;
+
+import com.vaadin.flow.component.*;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.shared.Registration;
+
+@Tag("demo-slider")
+@NpmPackage(value = "jquery", version = "1.9.1")
+@NpmPackage(value = "jquery-ui", version = "1.13.2")
+@CssImport("jquery-ui/dist/themes/base/jquery-ui.css")
+@JsModule("./src/component/slider/slider.js")
+public class Slider extends Component implements HasSize {
+    private static final String VALUE_PROPERTY = "value";
+    private static final String MIN_PROPERTY = "min";
+    private static final String MAX_PROPERTY = "max";
+
+    public Slider() {
+    }
+
+    public int getMin() {
+        return getElement().getProperty(MIN_PROPERTY, 0);
+    }
+
+    public void setMin(int min) {
+        getElement().setProperty(MIN_PROPERTY, min);
+    }
+
+    public int getMax() {
+        return getElement().getProperty(MAX_PROPERTY, 100);
+    }
+
+    public void setMax(int max) {
+        getElement().setProperty(MAX_PROPERTY, max);
+    }
+
+    @Synchronize("custom-slide-changed")
+    public int getValue() {
+        return getElement().getProperty(VALUE_PROPERTY, 0);
+    }
+
+    public void setValue(int value) {
+        getElement().setProperty(VALUE_PROPERTY, value);
+    }
+
+    public Registration addValueChangeListener(ComponentEventListener<SlideChangedEvent> listener) {
+        return addListener(SlideChangedEvent.class, listener);
+    }
+
+    @DomEvent("custom-slide-changed")
+    public static class SlideChangedEvent extends ComponentEvent<Slider> {
+
+        protected int value;
+
+        public SlideChangedEvent(Slider source, boolean fromClient,
+                                 @EventData("event.detail.value") int value) {
+            super(source, fromClient);
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+}
--- src/main/java/io/jmix/petclinic/view/pet/pet/PetDetailView.java
+++ src/main/java/io/jmix/petclinic/view/pet/pet/PetDetailView.java
@@ -1,11 +1,16 @@
 package io.jmix.petclinic.view.pet.pet;
 
+import com.vaadin.flow.component.notification.Notification;
+import io.jmix.flowui.Notifications;
+import io.jmix.flowui.component.formlayout.JmixFormLayout;
+import io.jmix.petclinic.component.Slider;
 import io.jmix.petclinic.entity.pet.Pet;
 
 import io.jmix.petclinic.view.main.MainView;
 
 import com.vaadin.flow.router.Route;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "pets/:id", layout = MainView.class)
 @ViewController("petclinic_Pet.detail")
@@ -13,4 +18,25 @@ import io.jmix.flowui.view.*;
 @EditedEntityContainer("petDc")
 @DialogMode
 public class PetDetailView extends StandardDetailView<Pet> {
+
+    @ViewComponent
+    private JmixFormLayout form;
+    @Autowired
+    private Notifications notifications;
+
+    @Subscribe
+    public void onInit(final InitEvent event) {
+        Slider slider = new Slider();
+        // slider.setWidth("20em");
+        slider.setMin(10);
+        slider.setMax(100);
+        slider.getStyle().setMarginTop("1em");
+
+        slider.addValueChangeListener(changedEvent -> {
+            notifications.create("New value is : " + changedEvent.getValue())
+                    .withPosition(Notification.Position.MIDDLE)
+                    .show();
+        });
+        form.add(slider);
+    }
 }
